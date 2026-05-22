fc355e7e921d786344500780be33603aa9cad1dd
prenger745 <5078317+prenger745@users.noreply.github.com>
2026-05-22 13:48:17 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ['three', 'three-globe', 'react-globe.gl', 'globe.gl'],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
