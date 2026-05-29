cebff5522c7ef1334e1ed0446c03d01d98cdc9ed
prenger745 <5078317+prenger745@users.noreply.github.com>
2026-05-29 12:56:48 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -823,7 +823,7 @@ export default function Page() {
                       <span style={{ color: "#4e6e58", marginLeft: "0.4rem" }}>({spot.grid})</span>
                     </div>
                     <div>
-                      SIG: <span style={{ color: "#00ff66" }}>{spot.snr}</span>
+                      SIG: <span style={{ color: "#a855f7" }}>{spot.snr}</span>
                     </div>
                   </div>
                 ))
